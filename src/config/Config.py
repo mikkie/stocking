f10c4e999a07919b7fabc7dc124996093e6279be
benjaminl <benjamin.li@ehealth.com>
2017-10-23 17:20:45 +0800

    init

--- src/config/Config.py
+++ src/config/Config.py
@@ -6,11 +6,15 @@ class Config(object):
       def __init__(self):
           self.__pKm5Change = 0.03 #振幅
           self.__superSold = 0.2 #超卖
+          self.__dbUrl = 'mysql://root:aqua@10.172.97.136/stocking?charset=utf8' #数据库地址
 
       def get_pKm5Change(self):
           return self.__pKm5Change
 
       def get_SuperSold(self):
           return self.__superSold  
+
+      def get_DBurl(self):
+          return self.__dbUrl  
 
       pass
--- src/main.py
+++ src/main.py
@@ -7,13 +7,13 @@ import config.Config as conf
 from sqlalchemy import create_engine
 
 
-def getData():
-    engine = create_engine('mysql://root:aqua@10.172.97.136/stocking?charset=utf8')
+def getData(setting):
+    engine = create_engine(setting.get_DBurl())
     df_hist_5 = pd.read_sql_table('km5', con=engine)
     return df_hist_5
 
 setting = conf.Config()
 dad = ds.DadStrategy()
-data = getData()
+data = getData(setting)
 dad.chooseStock(data,setting)
 
