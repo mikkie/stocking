62bf93e4e44c5e66d3c60bcb929a022c18741e82
benjaminl <benjamin.li@ehealth.com>
2017-11-29 10:14:33 +0800

    init

--- src/handlers/filtersV2/VolumeFilter.py
+++ src/handlers/filtersV2/VolumeFilter.py
@@ -14,6 +14,16 @@ class VolumeFilter(object):
              def cb(**kw):
                  return ts.get_hist_data(kw['kw']['code'])
              data['df_h'] = Utils.queryData('h_data_' + data['df_3m'].iloc[0]['code'],'code',data['engine'], cb, forceUpdate=config.get_updateToday(),code=data['df_3m'].iloc[0]['code'])   
+
+          df_h_90 = data['df_h'][config.get_FlatTrade()[1]:config.get_FlatTrade()[0]]
+          avg_90 = df_h_90['volume'].mean()
+
+          df_h_10 = data['df_h'][0:config.get_FlatTrade()[1]]
+          avg_10 = df_h_10['volume'].mean()
+
+          if avg_10 >= df_h_90 * 2:
+             return True
+           
           df_last_3h = data['df_h'][0:3]
           for index,row in df_last_3h.iterrows():
               if row['volume'] < row['v_ma10']:
