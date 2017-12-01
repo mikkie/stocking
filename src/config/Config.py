bfb021018674418eb97e0e7bdaa97db11c4a1b4d
benjaminl <benjamin.li@ehealth.com>
2017-12-01 16:55:47 +0800

    init

--- src/config/Config.py
+++ src/config/Config.py
@@ -15,10 +15,10 @@ class Config(object):
           self.__trendPeriod = 180 #主要分析范围 最近180天
           self.__dbUrl = 'mysql://root:aqua@127.0.0.1/stocking?charset=utf8' #数据库地址
           self.__turnOver = 3.00 #换手率
-          self.__updateToday = False #更新当前实时价格
-          self.__strategy = ['turnover','leftTrade'] #使用策略
+          self.__updateToday = True #更新当前实时价格
+          self.__strategy = ['turnover','volume','rightTrade'] #使用策略
           self.__kLineMA = [0.8,0.6] #K线超过MA5,MA10数量的百分比
-          self.__volume = [3,90,2] #量的突变
+          self.__volume = [3,90,1.3] #量的突变
 
       def get_pKm5Change(self):
           return self.__pKm5Change
