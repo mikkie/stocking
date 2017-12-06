e35f3d13acf88dd941e3fad5b7ecf5f9357ba3f4
benjaminl <benjamin.li@ehealth.com>
2017-12-06 17:18:51 +0800

    init

--- src/config/Config.py
+++ src/config/Config.py
@@ -12,7 +12,7 @@ class Config(object):
           self.__leftTrade = [5,4,3] #左侧交易
           self.__flatTrade = [90, 10, 1.2, 1.5] #长时间横盘后突破
           self.__longPeriod = 365 #1年
-          self.__trendPeriod = 180 #主要分析范围 最近180天
+          self.__trendPeriod = 120 #主要分析范围 最近120天
           self.__dbUrl = 'mysql://root:aqua@127.0.0.1/stocking?charset=utf8' #数据库地址
           self.__turnOver = 3.00 #换手率
           self.__updateToday = False #更新当前实时价格
