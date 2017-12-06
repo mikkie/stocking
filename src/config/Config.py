cd3940be48b77f8b9bab858fac8f20fe1dbe441c
benjaminl <benjamin.li@ehealth.com>
2017-12-06 16:51:58 +0800

    init

--- src/config/Config.py
+++ src/config/Config.py
@@ -18,7 +18,7 @@ class Config(object):
           self.__updateToday = False #更新当前实时价格
           self.__strategy = ['turnover','rightTrade','volume','bigMoney'] #使用策略
           self.__kLineMA = [0.8,0.6] #K线超过MA5,MA10数量的百分比
-          self.__volume = [3,90,1.3,1.2] #量的突变
+          self.__volume = [3,90,1.3,1.5] #量的突变
           self.__basics = [2017,3,20,1,5,15,30] #基础过滤
           self.__bigMoney = [1.2,400] #大单净流入
           self.__testCodes = ['600593'] #测试代码
