241af1b7b58c4dac9fe27f064f9edd103725bbc5
mikkie36 <lmj3648xm@hotmail.com>
2018-01-17 21:48:14 +0800

    init

--- src/tools/T7.py
+++ src/tools/T7.py
@@ -2,13 +2,17 @@
 __author__ = 'aqua'
 
 import tushare as ts
-import math
 import threading
 import time
 import pandas as pd
-from datetime import datetime
+from sqlalchemy import create_engine
+import sys
+sys.path.append('..')
+from config.Config import Config
 
 datas = {}
+setting = Config()
+engine = create_engine(setting.get_DBurl())
 
 def addData(df):
     for index,row in df.iterrows():
@@ -23,28 +27,24 @@ def addData(df):
               now = datas[code].append(row)
         else:
            datas[code] = pd.DataFrame([row])
-        print(open_type(datas[code]))
 
-def open_type(df):
-    row = df.iloc[0]
-    open_p = (float(row['price']) - float(row['pre_close'])) / float(row['pre_close']) * 100
-    #高开
-    if open_p > 4.0:
-       return 'H_open'
-    #平开
-    elif open_p > -1.0 and open_p < 1.0:
-         return 'F_open'
+def saveData():
+    for code in datas:
+        datas[code].to_sql('live_' + code,con=engine,if_exists='replace')
+    timer1 = threading.Timer(180, saveData)
+    timer1.start()
         
-           
-
-def calculate():
-    df = ts.get_realtime_quotes(['300698','000617','601375','002736','300708','603611'])
+def getData():
+    df = ts.get_realtime_quotes(['002736','600506','300698','300487','603098'])
     addData(df)
-    timer = threading.Timer(2, calculate)
+    timer = threading.Timer(2, getData)
     timer.start()
 
-timer = threading.Timer(2, calculate)
+timer = threading.Timer(2, getData)
 timer.start()
+
+timer1 = threading.Timer(180, saveData)
+timer1.start()
 
 while True:
       time.sleep(0.01)
