4c55e7bdfa5e0ac452b804feb53e2af5c0871b2d
benjaminl <benjamin.li@ehealth.com>
2018-01-19 17:03:37 +0800

    init

---
+++ src/t1/DataHolder.py
@@ -0,0 +1,11 @@
+# -*-coding=utf-8-*-
+__author__ = 'aqua'
+
+class DataHolder(object):
+      pass
+
+      def __init__(self):  
+          self.__data = {}
+
+      def get_data(self):
+          return self.__data
