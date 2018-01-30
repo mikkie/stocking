7bdf501c07cb9fe53d78d8ccd8329587b4242e93
benjaminl <benjamin.li@ehealth.com>
2018-01-30 15:36:17 +0800

    t10

--- src/t1/datas/Stock.py
+++ src/t1/datas/Stock.py
@@ -132,4 +132,4 @@ class Stock(object):
                 self.__data = self.__data.append(row) 
                 row_date = dt.datetime.strptime(row['date'] + ' ' + row['time'], '%Y-%m-%d %H:%M:%S') 
                 if (row_date - last_date).seconds > 3:
-                   MyLog.warn('%s get data is more than 3s' % row[code]) 
+                   MyLog.warn('%s get data is more than 3s' % row['code']) 
