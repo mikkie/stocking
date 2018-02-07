32018059eac7d6db8c5aa75013f4b3e0a70aad4b
benjaminl <benjamin.li@ehealth.com>
2018-02-07 15:10:53 +0800

    t10

--- src/config/Config.py
+++ src/config/Config.py
@@ -67,7 +67,7 @@ class Config(object):
               'big_money' : {
                   'amount' : 500000,
                   'volume' : 100000,
-                  'threshold' : 0.7
+                  'threshold' : 0.5
               },
               'speed' : {
                   'v30_ratio' : 0.55,
