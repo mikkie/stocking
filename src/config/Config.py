37a8b7d20c45e18a5ae452e377a6f3ccfdaccd38
benjaminl <benjamin.li@ehealth.com>
2018-02-11 14:36:46 +0800

    t10

--- src/config/Config.py
+++ src/config/Config.py
@@ -8,13 +8,13 @@ class Config(object):
           self.__timeStart = '14:25:00' #监控起始时间
           self.__pKm5Change = 0.01 #当日5分钟振幅
           self.__pKM3Change = 0.05 #3个月振幅
-          self.__superSold = [0.05,0.7] #超卖
+          self.__superSold = [0.05,0.2] #超卖
           self.__leftTrade = [5,4,3] #左侧交易
           self.__flatTrade = [90, 10, 1.2, 1.5] #长时间横盘后突破
           self.__longPeriod = 365 #1年
           self.__trendPeriod = 180 #主要分析范围 最近120天
           self.__dbUrl = 'mysql://root:aqua@127.0.0.1/stocking?charset=utf8' #数据库地址
-          self.__turnOver = 0.50 #换手率
+          self.__turnOver = 3 #换手率
           self.__updateToday = True #更新当前实时价格
           self.__strategy = ['turnover','rightTrade'] #使用策略
           self.__kLineMA = [10] #K线超过MA5,MA10数量
