d478898bfb7c7e94067ab173082908a0e864317c
benjaminl <benjamin.li@ehealth.com>
2018-03-19 16:31:40 +0800

    t10 new

--- src/extend/joinquant.py
+++ src/extend/joinquant.py
@@ -2,9 +2,9 @@
 import jqdata
 
 res = []
-df_all = get_all_securities(types=['stock'], date='2018-03-16')
+df_all = get_all_securities(types=['stock'], date='2018-03-19')
 for index,row in df_all.iterrows():
-    df_stock = get_price(index, end_date='2018-03-16', frequency='daily', fields=['close'], skip_paused=True, fq='pre', count=6)
+    df_stock = get_price(index, end_date='2018-03-19', frequency='daily', fields=['close'], skip_paused=True, fq='pre', count=3)
     count = 0
     pre_close = None
     for index_s,row_s in df_stock.iterrows():
@@ -14,6 +14,6 @@ for index,row in df_all.iterrows():
         if (row_s['close'] - pre_close) / pre_close * 100 > 9.3:
            count = count + 1
         pre_close = row_s['close']   
-    if count >= 1:
+    if count >= 2:
        res.append(index)
 print(res)
