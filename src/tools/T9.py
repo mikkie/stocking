40ac51c9f1ca947369262bac80ac42fefcb814a9
benjaminl <benjamin.li@ehealth.com>
2018-03-27 14:50:46 +0800

    t10 new

--- src/tools/T9.py
+++ src/tools/T9.py
@@ -15,7 +15,7 @@ from t1.analyze.Analyze import Analyze
 from t1.analyze.Concept import Concept
 from t1.analyze.NetMoney import NetMoney
 
-codes = ['603060']
+codes = ['300552']
 src_datas = {}
 datas = {}
 setting = Config()
