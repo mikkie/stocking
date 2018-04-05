e49b05dc3b436b82005d64eabfb9b61b787b6878
mikkie36 <lmj3648xm@hotmail.com>
2018-04-05 16:22:39 +0800

    t10 new

--- src/t1/trade/trade.py
+++ src/t1/trade/trade.py
@@ -3,6 +3,7 @@ __author__ = 'aqua'
 
 import easytrader
 import json
+import datetime as dt
 
 class Trade(object):
 
@@ -21,7 +22,23 @@ class Trade(object):
 
       def sell(self,code,price):
           isSelled = True
-          jsonData = self.__user.today_entrusts
-          j = json.loads(jsonData)
-                 
+          j = self.__user.today_entrusts
+          if len(j) > 0:
+             for stock in j:
+                 if stock['证券代码'] == code and stock['买卖标志'] == '卖出' and (int(stock['委托数量']) - int(stock['成交数量']) - int(stock['撤单数量'])) > 0:
+                    isSelled = False 
+                    ct = dt.datetime.strptime(stock['委托日期'] + ' ' + stock['委托时间'], '%Y%m%d %H:%M:%S')
+                    if (dt.datetime.now() - ct).seconds > 30: 
+                       self.__user.cancel_entrust(stock['委托序号'])
+          j = self.__user.position
+          if len(j) > 0:
+             for stock in j:
+                 if stock['证券代码'] == code and int(stock['股份可用']) > 0:
+                    isSelled = False
+                    self.__user.sell(code, price=price, amount=int(stock['股份可用']))   
+          return isSelled
+
+
+# trade = Trade()
+# trade.sell('002012',7.69)                 
           
