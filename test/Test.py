7c55fad878937936bb1a1595278a74f78c9a6f18
benjaminl <benjamin.li@ehealth.com>
2018-04-12 21:53:28 +0800

    t10 new

--- test/Test.py
+++ test/Test.py
@@ -3,6 +3,21 @@ __author__ = 'aqua'
 
 import pandas as pd
 import tushare as ts
+import datetime as dt
 
-df = ts.get_realtime_quotes(['sh','sz','hs300','sz50','zxb','cyb'])
-print(df)
+array = []
+df = ts.get_realtime_quotes(['600053'])
+now = dt.datetime.now()
+for i in range(3000):
+    array.append(df.iloc[0])
+print((dt.datetime.now() - now).seconds)    
+df = pd.DataFrame([df.iloc[0]])
+now = dt.datetime.now()
+for i in range(3000):
+    df.append(df.iloc[0])
+print((dt.datetime.now() - now).seconds)
+now = dt.datetime.now()
+all = None
+for i in range(3000):
+    all = pd.concat([df.iloc[0]])
+print((dt.datetime.now() - now).seconds)  
