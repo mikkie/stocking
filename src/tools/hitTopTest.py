09da645d5707c167caaec48cbef9508c835f47cd
benjaminl <benjamin.li@ehealth.com>
2018-04-17 12:05:30 +0800

    t13

--- src/tools/hitTopTest.py
+++ src/tools/hitTopTest.py
@@ -16,7 +16,7 @@ from t1.analyze.NewAnalyze import NewAnalyze
 from t1.analyze.Concept import Concept
 from t1.analyze.NetMoney import NetMoney
 
-codes = ['603607']
+codes = ['600239']
 src_datas = {}
 datas = {}
 setting = Config()
