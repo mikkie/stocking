651332f0c75be9460f49158f25bb31450cbe8f6f
benjaminl <benjamin.li@ehealth.com>
2018-05-04 15:25:42 +0800

    update next

--- src/tools/AutoSell.py
+++ src/tools/AutoSell.py
@@ -19,7 +19,7 @@ import os
 from apscheduler.schedulers.blocking import BlockingScheduler
 import datetime as dt
 
-codeList = ['002417']
+codeList = ['000670','002848']
 setting = Config()
 mockTrade = MockTrade()
 engine = create_engine(setting.get_DBurl())
