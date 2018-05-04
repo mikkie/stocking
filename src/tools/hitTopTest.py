80756e8e8cf5c436787492b79c30cb0eaf4723c5
benjaminl <benjamin.li@ehealth.com>
2018-05-04 10:26:46 +0800

    update cookie

--- src/tools/hitTopTest.py
+++ src/tools/hitTopTest.py
@@ -16,7 +16,7 @@ from t1.analyze.NewAnalyze import NewAnalyze
 # from t1.analyze.Concept import Concept
 # from t1.analyze.NetMoney import NetMoney
 
-codes = ['300312']
+codes = ['300611']
 src_datas = {}
 datas = {}
 setting = Config()
