c033991be4eedfb79e2a8a1057c642348869e1a4
benjaminl <benjamin.li@ehealth.com>
2018-05-31 21:24:25 +0800

    ignore 160 day no 10%

--- src/doc/readMe.txt
+++ src/doc/readMe.txt
@@ -24,6 +24,7 @@ main.py init 300 c 节能
 1.filter by common.sql (price < 30)
 2.filter by twodayten.py (keep yesterday 10%)
 3.filter by NewStock.py
-4.filter by xsg.js
-5.filter by yesterday buy
-6.filter by yg.py
+4.filter by bull2.py 
+5.filter by xsg.js
+6.filter by yesterday buy
+7.filter by yg.py
--- src/extend/bull2.py
+++ src/extend/bull2.py
@@ -4,33 +4,23 @@ import numpy as np
 import talib as ta
 
 
-startDate = '2018-04-24'
-res = []
+startDate = '2018-05-31'
+failed = []
 df_all = get_all_securities(types=['stock'], date=startDate)
 for index,row in df_all.iterrows():
     try:
-        df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','low'], skip_paused=True, fq='pre', count=60)
-        if len(df_stock) < 10:
-           continue    
-        countContinue10 = 0    
-        pre_close = None
+        df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','high_limit','low'], skip_paused=True, fq='pre', count=180)
         flag = False
+        count = 0
         for index_s,row_s in df_stock.iterrows():
-            if pre_close is None:
-               pre_close = row_s['close']
-               continue
-            if (row_s['close'] - pre_close) / pre_close * 100 >= 9.93:
-               countContinue10 = countContinue10 + 1
-               if countContinue10 >= 2:
+            if row_s['close'] == row_s['high_limit']:
+               count = count + 1
+               if count >= 1:  
                   flag = True
-                  break 
-            else:
-                countContinue10 = 0    
-            pre_close = row_s['close']
-        if flag:
-           index = index.replace('.XSHE','').replace('.XSHG','') 
-           res.append(index)
+                  break
+        if not flag:     
+           failed.append(index.replace('.XSHE','').replace('.XSHG',''))
     except Exception as e:
            print(e)
         
-print(res)            
+print(failed)        
