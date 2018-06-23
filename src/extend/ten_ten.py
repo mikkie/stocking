3727d66ef316df30e6dde1f3aaf7125bea617bc7
mikkie36 <lmj3648xm@hotmail.com>
2018-06-23 10:23:03 +0800

    na

---
+++ src/extend/ten_ten.py
@@ -0,0 +1,81 @@
+# 启动的
+import jqdata
+import numpy as np
+import talib as ta
+import datetime
+
+
+startDate = '2018-06-20'
+hitDate='2018-06-19'
+winStop = 3.0
+lossStop = -2.0
+res = []
+failedRes = []
+ten_ten_res = []
+ten_ten_failedRes = []
+failed_ten_res = []
+failed_ten_failedRes = []
+low_list = []
+
+ten_ten_df = []
+failed_ten_df = []
+ten_failed_df = []
+failed_failed_df = []
+today = datetime.date.today()
+
+df_all = get_all_securities(types=['stock'], date=startDate)
+for index,row in df_all.iterrows():
+    try:
+        df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','high_limit','low','pre_close'], skip_paused=True, fq='pre', count=3)
+        stock = get_security_info(index)
+        if (today - stock.start_date).days < 20: 
+            continue 
+        zero = df_stock.iloc[-3] 
+        first = df_stock.iloc[-2]
+        last = df_stock.iloc[-1]
+        index = index.replace('.XSHE','').replace('.XSHG','') 
+        if first['high'] == first['high_limit']:
+           if first['close'] == first['high_limit']:
+              if zero['close'] == zero['high_limit']: 
+                 ten_ten_df.append(index)
+              else:
+                   failed_ten_df.append(index)
+           else:
+                if zero['close'] == zero['high_limit']: 
+                   ten_failed_df.append(index)
+                else:
+                    failed_failed_df.append(index)   
+        if (first['close'] == first['high_limit']):
+            if (last['high'] - last['pre_close']) / last['pre_close'] * 100 >= winStop:
+               low_p = (last['low'] - last['pre_close']) / last['pre_close'] * 100  
+               if low_p < lossStop:
+                  low_list.append(index) 
+               res.append(index)
+               if zero['close'] == zero['high_limit']:
+                  ten_ten_res.append(index)
+               else:
+                   failed_ten_res.append(index)    
+            else:
+                failedRes.append(index) 
+                if zero['close'] == zero['high_limit']:
+                   ten_ten_failedRes.append(index)
+                else:
+                    failed_ten_failedRes.append(index)   
+    except Exception as e:
+           print(e)
+        
+
+print(u'%s号打板,%s号卖出' % (hitDate,startDate))
+ratio = float(len(ten_ten_df)) / float(len(ten_ten_df) + len(ten_failed_df))
+total = len(ten_ten_res) + len(ten_ten_failedRes) 
+print(u'ten_ten最高涨幅>%.2f%%的概率= %.2f%%' % (winStop,ratio * float(len(ten_ten_res)) / float(total) * 100))
+ratio = float(len(failed_ten_df)) / float(len(failed_ten_df) + len(failed_failed_df))
+total = len(failed_ten_res) + len(failed_ten_failedRes) 
+print(u'failed_ten最高涨幅>%.2f%%的概率= %.2f%%' % (winStop,ratio * float(len(failed_ten_res)) / float(total) * 100))
+ratio = float(len(ten_ten_df) + len(failed_ten_df)) / float(len(ten_ten_df) + len(failed_ten_df) + len(ten_failed_df) + len(failed_failed_df))
+total = len(res) + len(failedRes) 
+print(u'最高涨幅>%.2f%%的概率= %.2f%%' % (winStop,ratio * float(len(res)) / float(total) * 100))
+print(ten_ten_df)
+# print(u'最高涨幅<%.2f%%的概率= %.2f%%' % (winStop,float(len(failedRes)) / float(total) * 100))
+# print(failedRes)
+# print(u'最高涨幅>%.2f%%时,最低跌幅超过%.2f%%的概率=%.2f%%' % (winStop,lossStop,float(len(low_list)) / float(len(res)) * 100))
