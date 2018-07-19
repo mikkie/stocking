d6f94deacf72a6b583097ce4f86fb9cde5188351
benjaminl <benjamin.li@ehealth.com>
2018-07-19 17:05:04 +0800

    na

--- src/extend/bull2.py
+++ src/extend/bull2.py
@@ -10,14 +10,18 @@ df_all = get_all_securities(types=['stock'], date=startDate)
 for index,row in df_all.iterrows():
     try:
         df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','high_limit','low'], skip_paused=True, fq='pre', count=90)
+        flagA = False
         flag = False
         count = 0
         for index_s,row_s in df_stock.iterrows():
             if row_s['close'] == row_s['high_limit']:
-               count = count + 1
-               if count >= 1:  
-                  flag = True
+               if flagA:
+                  flag = True 
                   break
+               else:
+                   flagA = True
+            else:
+                flagA = False         
         if not flag:     
            failed.append(index.replace('.XSHE','').replace('.XSHG',''))
     except Exception as e:
