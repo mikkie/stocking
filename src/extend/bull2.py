13a3f0bfd324068cf7d00e9d4456476eb3483a14
mikkie36 <lmj3648xm@hotmail.com>
2018-08-05 09:22:22 +0800

    na

--- src/extend/bull2.py
+++ src/extend/bull2.py
@@ -4,25 +4,24 @@ import numpy as np
 import talib as ta
 
 
-startDate = '2018-07-27'
+startDate = '2018-08-03'
 failed = []
 df_all = get_all_securities(types=['stock'], date=startDate)
 for index,row in df_all.iterrows():
     try:
-        df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','high_limit','low'], skip_paused=True, fq='pre', count=180)
-        flagA = False
-        flag = False
+        df_stock = get_price(index, end_date=startDate, frequency='daily', fields=['open','close','high','high_limit','low'], skip_paused=True, fq='pre', count=30)
         count = 0
+        flag = False
         for index_s,row_s in df_stock.iterrows():
             if row_s['close'] == row_s['high_limit']:
-               if flagA:
+               if count == 3:
                   flag = True 
                   break
                else:
-                   flagA = True
+                   count += 1
             else:
-                flagA = False         
-        if not flag:     
+                count = 0         
+        if flag:     
            failed.append(index.replace('.XSHE','').replace('.XSHG',''))
     except Exception as e:
            print(e)
