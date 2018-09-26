7705abff4ee25e9105bfbbe86d759908c30ac3df
mikkie36 <lmj3648xm@hotmail.com>
2018-09-26 19:01:09 +0800

    na

--- src/extend/twodayten.py
+++ src/extend/twodayten.py
@@ -3,7 +3,7 @@ import jqdata
 import numpy as np
 import talib as ta
 
-startDate = '2018-09-25'
+startDate = '2018-09-26'
 res = []
 df_all = get_all_securities(types=['stock'], date=startDate)
 for index,row in df_all.iterrows():
