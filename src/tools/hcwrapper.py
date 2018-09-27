79e4e0d7b32f9b4095d1f95f8de7316ea29cbad6
benjaminl <benjamin.li@ehealth.com>
2018-09-27 17:19:03 +0800

    na

--- src/tools/hcwrapper.py
+++ src/tools/hcwrapper.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
    load_dir = '../../data/excels'
    compare_dir = '../../data/compare'
 
-#    hcdata.removeall(load_dir)
-#    code_list = download.download(code_pre_close_map, date, load_dir)
+   hcdata.removeall(load_dir)
+   code_list = download.download(code_pre_close_map, date, load_dir)
    df_list = hcdata.loaddata(load_dir, save=False)
    result = hitTopTest2.start_test_by_df(df_list)
    if len(result) > 0:
@@ -32,7 +32,7 @@ if __name__ == '__main__':
          for res in result:
              for df in compare_list:
                  if df.iloc[0]['code'] == res[0]:
-                    print('[%s] buy price=%s, open=%s, close=%s, low=%s, max=%s' % (res[0], res[1], df.iloc[0]['price'], df.iloc[-1]['price'], df['price'].max(), df['price'].min()))
+                    print('[%s] buyed price=%s, next day: open=%s, close=%s, low=%s, high=%s' % (res[0], res[1], df.iloc[0]['price'], df.iloc[-1]['price'], df['price'].min(), df['price'].max()))
                     continue 
           
            
