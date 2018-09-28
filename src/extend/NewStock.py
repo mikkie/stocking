45425bf7621618ca904cb87536ac7c64daa5ce58
mikkie36 <lmj3648xm@hotmail.com>
2018-09-28 18:41:48 +0800

    na

--- src/extend/NewStock.py
+++ src/extend/NewStock.py
@@ -6,7 +6,7 @@ import datetime
 
 res = []
 today = datetime.date.today()
-startDate = '2018-09-26'
+startDate = '2018-09-28'
 df_all = get_all_securities(types=['stock'], date=startDate)
 for index,row in df_all.iterrows():
     try:
