0ac593c771379b0f05605e95f1a2f4e14ab39be3
benjaminl <benjamin.li@ehealth.com>
2018-09-28 17:11:42 +0800

    na

--- src/hyconcept/HYConceptParser.py
+++ src/hyconcept/HYConceptParser.py
@@ -4,16 +4,22 @@ __author__ = 'aqua'
 from lxml import etree
 
 class HYConceptParser():
-      pass
+      
+    
+      def get_page_info(self, html):
+          page = etree.HTML(html)
+          span = page.xpath('//span[@class="page_info"]')  
+          total = span[0].text
+          return int(total.split('/')[1])
 
       def parse(self,html):
           codes = []
           page = etree.HTML(html)
-          span = page.xpath('//div[@class="board-hq"]/h3/span')
-          cont = span[0].text
+      #     span = page.xpath('//div[@class="board-hq"]/h3/span')
+      #     cont = span[0].text
           trs = page.xpath('//table[@class="m-table m-pager-table"]/tbody/tr')
           i = 0
-          while i < len(trs) and i < 5:
+          while i < len(trs):
                 code = trs[i].getchildren()[1].getchildren()[0].text
                 codes.append(code)
                 i = i + 1
