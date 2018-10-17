a810bb2e12423d08b849b039f86382dd6af8a622
benjaminl <benjamin.li@ehealth.com>
2018-10-17 11:06:17 +0800

    na

--- src/t1/trade/ProxyManager.py
+++ src/t1/trade/ProxyManager.py
@@ -12,7 +12,7 @@ import requests
 import json
 
 class ProxyManager(object):
-     
+    
       PROXY_URL = 'http://webapi.http.zhimacangku.com/getip?num={proxy_size}&type=2&pro=0&city=0&yys=100017&port=11&time=4&ts=1&ys=1&cs=1&lb=1&sb=0&pb=45&mr=1&regions=310000,320000,330000,350000,440000'
 
       def __init__(self, proxy_size):
@@ -39,11 +39,11 @@ class ProxyManager(object):
                  MyLog.info(proxy_list)
              except Exception as e:
                     MyLog.error(e) 
-          if len(proxy_list) == self.proxy_size:
+          if len(proxy_list) > 0:
              for proxy in proxy_list: 
                  self.current_list.append(proxy)
           else:
-              MyLog.error('can not get proxy')
+               MyLog.error('can not get proxy')
 
 
       def append_proxy(self, size):
@@ -56,7 +56,7 @@ class ProxyManager(object):
               MyLog.info(proxy_list)
           except Exception as e:
                  MyLog.error(e) 
-          if len(proxy_list) == size:
+          if len(proxy_list) > 0:
              for proxy in proxy_list: 
                  self.current_list.append(proxy)
           else:
