49e64ac3c8639f5d0e5ede44bf570efe4c77c7c6
benjaminl <benjamin.li@ehealth.com>
2018-10-23 17:28:11 +0800

    na

--- test/tiger.py
+++ test/tiger.py
@@ -1,5 +1,11 @@
 # -*-coding=utf-8-*-
 __author__ = 'aqua'
 
-import tushare as ts
-print(ts.get_realtime_quotes(['603999']))
+import pymongo
+import json
+
+client = pymongo.MongoClient('mongodb://pig:pigpiggo@localhost:27017/stocking')
+for doc in client['stocking']['config'].find({'t1' : {'$exists' : True}}):
+    print(doc['t1']['ydls'])
+    print(doc['t1']['trade']['enable'])
+    print(doc['t1']['ydls']['stop_p'])
