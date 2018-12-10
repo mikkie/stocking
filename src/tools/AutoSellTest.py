98fd898d4d426027786077c89ef361b8c30b7e01
benjaminl <benjamin.li@ehealth.com>
2018-12-10 17:20:45 +0800

    test sell

--- src/tools/AutoSellTest.py
+++ src/tools/AutoSellTest.py
@@ -16,6 +16,7 @@ from t1.analyze.Concept import Concept
 from t1.analyze.NetMoney import NetMoney
 from t1.trade.MockTrade import MockTrade
 from t1.analyze.SellAnalyze import SellAnalyze
+from utils.Utils import Utils
 
 codes = ['300116']
 src_datas = {}
@@ -35,13 +36,6 @@ mockTrade.relogin()
 # net = netMoney.getNetMoneyRatio()
 # zs = ts.get_realtime_quotes(['sh','sz','hs300','sz50','zxb','cyb'])
 
-for code in codes:
-    try:
-       src_datas[code] = pd.read_sql_table('live_' + code, con=engine)
-    except Exception as e:
-           MyLog.error('read mock data error \n')
-           MyLog.error(str(e) +  '\n')   
-
 def run(i):
     df = pd.DataFrame()
     for code in src_datas:
@@ -51,8 +45,16 @@ def run(i):
        dh.addSellData(df)
        analyze.calcMain(None,dh)
 
-for i in range(5200):
-    run(i)
 
+@Utils.printperformance
+def start_test_by_df(df_list):
+    for df in df_list:
+        try:
+            code = df.iloc[0]['code']
+            src_datas[code] = df
+        except Exception as e:
+               MyLog.error('read mock data error \n')
+               MyLog.error(str(e) +  '\n') 
+    for i in range(5200):
+        run(i)
 
-input('please enter to exit')       
